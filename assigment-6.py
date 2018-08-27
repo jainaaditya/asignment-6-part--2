bf6d3571a82575cc456ff465fb9541c120258e38
jainaaditya <42334537+jainaaditya@users.noreply.github.com>
2018-08-27 23:54:54 +0530

    Update assigment-6.py

--- assigment-6.py
+++ assigment-6.py
@@ -1,13 +1,29 @@
-#ques1
-dic={1:'abc',2:'lmn',3:'pqr',4:'xyz'}
-list1=[]
-for i in dic.keys():
-    list1.append(i)
-print(list1)
+#Q1.-Get Keys Corresponding to a Value in User Defined Dictionary
+dicti={}
+n=int(input("Enter number of items: "))
+for x in range(n):
+    key=input("Enter key ")
+    value=input("Enter value ")
+    dicti[key]=value
+print(dicti)
+val=input("Enter value to find")
+for y,z in dicti.items():
+    if(z==val):
+        print("Key of value",val,"is",y)
 
-#ques2
-dic={'aditya':{'maths':'80','sci':'85'},
-     'abhi':{'maths':'81','sci':'90'}}
-print(dic['aditya']['maths'],dic['aditya']['sci'])
-print(dic['abhi']['maths'],dic['abhi']['sci'])
-     
+#Q2.- Nested Dictionary
+a={}
+n=int(input("Enter number of items: "))
+for x in range(n):
+    name=input("Enter name ")
+    b={}
+    marks1=int(input("Enter marks in C++ "))
+    marks2=int(input("Enter marks in CST "))
+    marks3=int(input("Enter marks in CN "))
+    b['C++']=marks1
+    b['CN']=marks2
+    b['CST']=marks3
+    a[name]=b
+print(a)
+s=input("Enter name of student :")
+print(a[s])
